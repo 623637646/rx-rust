3e4f5fbea84e421cb60fe9c75d0fd8cebc81a875
ya.wang <ya.wang@okg.com>
2024-09-05 22:27:40 +0800

    Comments

--- src/observable/mod.rs
+++ src/observable/mod.rs
@@ -9,6 +9,7 @@ use crate::observer::Observer;
 pub trait Observable<'a, T, E> {
     /// Subscribes an observer to this observable.
     /// Returns a cancellable that can be used to cancel the subscription.
+    /// The O type must be 'static because it will be stored in hot observables or pass to `subscribe_handler` of `Create`.
     fn subscribe<O>(&'a self, observer: O) -> impl Cancellable
     where
         O: Observer<T, E> + 'static;
