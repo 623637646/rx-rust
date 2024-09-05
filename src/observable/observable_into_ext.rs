24df7d3bc43ccd7a06554116f5175e32611d0657
ya.wang <ya.wang@okg.com>
2024-09-05 22:57:06 +0800

    observable_into_ext

--- src/observable/observable_into_ext.rs
+++ src/observable/observable_into_ext.rs
@@ -1,7 +1,7 @@
 use super::Observable;
 
 pub trait ObservableIntoExt<T, E> {
-    fn into_observable(self) -> impl for<'b> Observable<'b, T, E>;
+    fn into_observable(self) -> impl for<'a> Observable<'a, T, E>;
 }
 
 impl<T, E, O> ObservableIntoExt<T, E> for O
@@ -11,4 +11,44 @@ where
     fn into_observable(self) -> impl for<'b> Observable<'b, T, E> {
         self
     }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::cancellable::non_cancellable::NonCancellable;
+    use crate::observable::observable_into_ext::ObservableIntoExt;
+    use crate::observable::Observable;
+    use crate::observer::{Event, Observer, Terminated};
+    use crate::operators::create::Create;
+    use crate::operators::just::Just;
+    use crate::utils::test_helper::ObservableChecker;
+
+    #[test]
+    fn test_create() {
+        let value = 123;
+        let observable = Create::new(|observer: Box<dyn Observer<i32, String>>| {
+            observer.on(Event::Next(value));
+            observer.on(Event::Terminated(Terminated::Completed));
+            NonCancellable
+        })
+        .into_observable();
+
+        let checker = ObservableChecker::new();
+        observable.subscribe(checker.clone());
+        assert!(checker.is_values_matched(&[&value]));
+        assert!(checker.is_completed());
+    }
+
+    // #[test]
+    // fn test_just() {
+    //     let value = 123;
+    //     let observable = Just::new(value);
+    //     let observable = observable.into_observable();
+
+    //     let checker = ObservableChecker::new();
+    //     observable.subscribe(checker.clone());
+    //     assert!(checker.is_values_matched(&[&value]));
+    //     assert!(checker.is_completed());
+    // }
 }
