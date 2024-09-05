8440fdd054a0d4b8a8946ae83fd07e5b9423d154
ya.wang <ya.wang@okg.com>
2024-09-05 17:57:12 +0800

    comments

--- src/cancellable/mod.rs
+++ src/cancellable/mod.rs
@@ -1,6 +1,7 @@
 pub mod anonymous_cancellable;
 pub mod non_cancellable;
 
+/// A trait for objects that can be cancelled. This is useful for cancelling subscriptions.
 pub trait Cancellable {
     fn cancel(self);
 }
--- src/cancellable/non_cancellable.rs
+++ src/cancellable/non_cancellable.rs
@@ -1,5 +1,6 @@
 use crate::cancellable::Cancellable;
 
+/// A cancellable that does nothing when cancelled.
 pub struct NonCancellable;
 
 impl Cancellable for NonCancellable {
