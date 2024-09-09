60e1e59ac424d23854c5b13238907110b5086322
ya.wang <ya.wang@okg.com>
2024-09-09 17:50:21 +0800

    Comments of anonymous_observer.rs

--- src/observer/anonymous_observer.rs
+++ src/observer/anonymous_observer.rs
@@ -1,19 +1,23 @@
 use super::{Event, Observer};
 
-/// An observer that wraps a closure.
-/// Example:
-/// ```rust
-/// use rx_rust::observable::Observable;
-/// use rx_rust::observer::anonymous_observer::AnonymousObserver;
-/// use rx_rust::observer::Event;
-/// use rx_rust::operators::just::Just;
-/// use rx_rust::utils::never::Never;
-/// let observable = Just::new(123);
-/// let observer = AnonymousObserver::new(|e: Event<&i32, Never>| {
-///     println!("{:?}", e);
-/// });
-/// observable.subscribe(observer);
-/// ```
+/**
+An observer that wraps a closure.
+
+Example:
+```rust
+use rx_rust::observable::Observable;
+use rx_rust::observer::anonymous_observer::AnonymousObserver;
+use rx_rust::observer::Event;
+use rx_rust::operators::just::Just;
+use rx_rust::utils::never::Never;
+let observable = Just::new(123);
+let observer = AnonymousObserver::new(|e: Event<&i32, Never>| {
+    println!("{:?}", e);
+});
+observable.subscribe(observer);
+```
+*/
+
 pub struct AnonymousObserver<F> {
     on_event: F,
 }
