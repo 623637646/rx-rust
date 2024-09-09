8cf99c18250f10d0cbdc6f5c0f0390a6f644511e
ya.wang <ya.wang@okg.com>
2024-09-09 17:29:25 +0800

    Comments of observable_into_ext.rs

--- src/observable/observable_into_ext.rs
+++ src/observable/observable_into_ext.rs
@@ -1,6 +1,25 @@
 use super::Observable;
 
-/// This trait is used to convert any type that implements `Observable` into `impl Observable<T, E>`.
+/**
+This trait is used to convert any type that implements `Observable` into `impl Observable<T, E>`.
+
+Example:
+```rust
+use rx_rust::{
+    observable::{
+        observable_into_ext::ObservableIntoExt,
+        observable_subscribe_ext::ObservableSubscribeExt,
+    },
+    operators::just::Just,
+};
+let just = Just::new(123);
+let observable = just.into_observable();
+observable.subscribe_on_next(|value| {
+    println!("value: {}", value);
+});
+```
+ */
+
 pub trait ObservableIntoExt<T, E> {
     /// Converts any type that implements `Observable` into `impl Observable<T, E>`.
     fn into_observable(self) -> impl Observable<T, E>;
@@ -10,22 +29,6 @@ impl<T, E, O> ObservableIntoExt<T, E> for O
 where
     O: Observable<T, E>,
 {
-    /// Converts any type that implements `Observable` into `impl Observable<T, E>`.
-    /// Example:
-    /// ```rust
-    /// use rx_rust::{
-    ///     observable::{
-    ///         observable_into_ext::ObservableIntoExt,
-    ///         observable_subscribe_ext::ObservableSubscribeExt,
-    ///     },
-    ///     operators::just::Just,
-    /// };
-    /// let just = Just::new(123);
-    /// let observable = just.into_observable();
-    /// observable.subscribe_on_next(|value| {
-    ///     println!("value: {}", value);
-    /// });
-    /// ```
     fn into_observable(self) -> impl Observable<T, E> {
         self
     }
