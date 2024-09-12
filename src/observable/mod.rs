13dca68486498c95bde6b461d6daf04f5c6935c9
ya.wang <ya.wang@okg.com>
2024-09-12 11:30:20 +0800

    Comments

--- src/observable/mod.rs
+++ src/observable/mod.rs
@@ -8,5 +8,6 @@ use crate::{observer::Observer, subscription::Subscription};
 pub trait Observable<'a, T, E> {
     /// Subscribes an observer to this observable. Returns a Subscription that can be unsubscribed.
     /// The Observer must be 'static because it will be stored in hot observables or pass to `subscribe_handler` of `Create`.
+    /// We use Subscription instead of type like `impl Cancellable`, because we need to implement Drop for Subscription to cancel the subscription when the Subscription is dropped. It's not possible to implement Drop for a trait object.
     fn subscribe(&'a self, observer: impl Observer<T, E> + 'static) -> Subscription;
 }
