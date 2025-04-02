9f97a211a27161abbaa04a4a196b8013021ef156
Yanni Wang <wy19900729@gmail.com>
2025-04-02 22:47:58 +0800

    Tests of Throw

--- src/operators/throw.rs
+++ src/operators/throw.rs
@@ -43,29 +43,101 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::utils::checking_observer::CheckingObserver;
+    use crate::{
+        observable::observable_subscribe_ext::ObservableSubscribeExt,
+        utils::checking_observer::CheckingObserver,
+    };
 
     #[test]
     fn test_error() {
-        let observable = Throw::new(333);
+        let observable = Throw::new(111);
         let checker = CheckingObserver::new();
-        observable.subscribe(checker.clone());
+
+        let subscription = observable.subscribe(checker.clone());
         assert!(checker.is_values_matched(&[]));
-        assert!(checker.is_error(333));
+        assert!(checker.is_error(111));
+
+        drop(subscription); // keep the subscription alive
     }
 
     #[test]
-    fn test_multiple_subscribe() {
-        let observable = Throw::new("My error");
+    fn test_ref() {
+        let error = 111;
 
+        let observable = Throw::new(&error);
         let checker = CheckingObserver::new();
-        observable.clone().subscribe(checker.clone());
-        assert!(checker.is_values_matched(&[]));
-        assert!(checker.is_error("My error"));
 
-        let checker = CheckingObserver::new();
-        observable.subscribe(checker.clone());
+        let subscription = observable.subscribe(checker.clone());
         assert!(checker.is_values_matched(&[]));
-        assert!(checker.is_error("My error"));
+        assert!(checker.is_error(&error));
+
+        drop(subscription); // keep the subscription alive
+    }
+
+    #[test]
+    fn test_mut_ref() {
+        let mut error = 111;
+
+        let observable = Throw::new(&mut error);
+        let checker: CheckingObserver<i32, i32> = CheckingObserver::new();
+
+        let checker_cloned = checker.clone();
+        let subscription = observable.subscribe_on(
+            |_| unreachable!(),
+            |terminal| match terminal {
+                Terminal::Completed => unreachable!(),
+                Terminal::Error(error) => {
+                    checker_cloned.on_terminal(Terminal::Error(*error));
+                    *error = 222;
+                }
+            },
+        );
+
+        assert!(checker.is_values_matched(&[]));
+        assert!(checker.is_error(111));
+        assert_eq!(error, 222);
+
+        drop(subscription); // keep the subscription alive
+    }
+
+    #[tokio::test]
+    async fn test_async() {
+        let observable = Throw::new(111);
+        let checker = CheckingObserver::new();
+
+        let checker_cloned = checker.clone();
+        let handle = tokio::spawn(async move { observable.subscribe(checker_cloned) });
+        let subscription = handle.await.unwrap();
+        assert!(checker.is_values_matched(&[]));
+        assert!(checker.is_error(111));
+
+        let handle = tokio::spawn(async { subscription.unsubscribe() });
+        handle.await.unwrap();
+        assert!(checker.is_values_matched(&[]));
+        assert!(checker.is_error(111));
+    }
+
+    #[test]
+    fn test_subscribe_by_different_observer() {
+        let observable = Throw::new(111);
+        let checker_1 = CheckingObserver::new();
+        let checker_2 = CheckingObserver::new();
+
+        // Custom operations
+        let observable_1 = observable.clone();
+        let observable_2 = observable_1.clone();
+
+        let subscription_1 = observable_1.subscribe(checker_1.clone());
+
+        let (on_next, on_terminal) = checker_2.fn_for_subscribe_on();
+        let subscription_2 = observable_2.subscribe_on(on_next, on_terminal);
+
+        assert!(checker_1.is_values_matched(&[]));
+        assert!(checker_1.is_error(111));
+        assert!(checker_2.is_values_matched(&[]));
+        assert!(checker_1.is_error(111));
+
+        drop(subscription_1); // keep the subscription alive
+        drop(subscription_2); // keep the subscription alive
     }
 }
