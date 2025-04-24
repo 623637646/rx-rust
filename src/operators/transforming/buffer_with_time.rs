6d2ee72f6627655a3a98abdf19a1c26e0092ffdd
Yanni Wang <wy19900729@gmail.com>
2025-04-24 17:05:25 +0800

    Improvement of BufferWithTime. Make the conditions less.

--- src/operators/transforming/buffer_with_time.rs
+++ src/operators/transforming/buffer_with_time.rs
@@ -1,12 +1,14 @@
 use crate::{
-    observable::{Observable, observable_ext::ObservableExt},
-    observer::Observer,
-    operators::creating::interval::Interval,
+    observable::Observable,
+    observer::{Observer, Terminal},
     scheduler::Scheduler,
     subscription::Subscription,
 };
 use educe::Educe;
-use std::time::Duration;
+use std::{
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
 #[derive(Educe)]
 #[educe(Debug, Clone)]
@@ -26,19 +28,64 @@ impl<OE, S> BufferWithTime<OE, S> {
     }
 }
 
-impl<'sub, T, E, OE, S> Observable<'static, 'sub, Vec<T>, E> for BufferWithTime<OE, S>
+impl<'or, 'sub, T, E, OE, S> Observable<'static, 'sub, Vec<T>, E> for BufferWithTime<OE, S>
 where
     T: Send + 'static,
-    E: 'static,
-    OE: Observable<'static, 'sub, T, E>,
+    OE: Observable<'or, 'sub, T, E>,
     S: Scheduler,
 {
     fn subscribe(self, observer: impl Observer<Vec<T>, E> + Send + 'static) -> Subscription<'sub> {
-        let boundary = Interval::new(self.time_pan, self.scheduler, Some(self.time_pan))
-            .map_value_to_void()
-            .map_infallible_to_error();
-        let buffer = self.source.buffer(boundary);
-        buffer.subscribe(observer)
+        let observer = BufferWithTimeObserver {
+            observer: Arc::new(Mutex::new(Some(observer))),
+            values: Arc::new(Mutex::new(Vec::default())),
+        };
+        let observer_cloned = observer.clone();
+        let disposal = self.scheduler.schedule_period(
+            move |_| {
+                if let Some(observer) = observer_cloned.observer.lock().unwrap().as_mut() {
+                    let mut values = observer_cloned.values.lock().unwrap();
+                    observer.on_next(std::mem::take(&mut values));
+                }
+            },
+            self.time_pan,
+            Some(self.time_pan),
+        );
+        self.source.subscribe(observer) + disposal
+    }
+}
+
+#[derive(Educe)]
+#[educe(Debug, Clone)]
+pub struct BufferWithTimeObserver<T, OR> {
+    observer: Arc<Mutex<Option<OR>>>,
+    values: Arc<Mutex<Vec<T>>>,
+}
+
+impl<T, E, OR> Observer<T, E> for BufferWithTimeObserver<T, OR>
+where
+    OR: Observer<Vec<T>, E>,
+{
+    fn on_next(&mut self, value: T) {
+        self.values.lock().unwrap().push(value);
+    }
+
+    fn on_terminal(self, terminal: Terminal<E>) {
+        match terminal {
+            Terminal::Completed => {
+                if let Some(mut observer) = self.observer.lock().unwrap().take() {
+                    let mut values = self.values.lock().unwrap();
+                    if !values.is_empty() {
+                        observer.on_next(std::mem::take(&mut values));
+                    }
+                    observer.on_terminal(Terminal::Completed);
+                }
+            }
+            Terminal::Error(error) => {
+                if let Some(observer) = self.observer.lock().unwrap().take() {
+                    observer.on_terminal(Terminal::Error(error));
+                }
+            }
+        }
     }
 }
 
