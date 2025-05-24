f39eaebe417077592a7aa5a3506a3ed8fccbba2d
Yanni Wang <wy19900729@gmail.com>
2025-05-24 17:58:14 +0800

    Revert "Use T2 instead of () for boundary of Buffer."
    This reverts commit f30ec0731a764f098423b390f4680185d7798646.

--- src/observable/observable_ext.rs
+++ src/observable/observable_ext.rs
@@ -24,10 +24,10 @@ use crate::{
 use std::{convert::Infallible, time::Duration};
 
 pub trait ObservableExt: Sized {
-    fn buffer<'or, 'sub, T, T2, E, OE2>(self, boundary: OE2) -> Buffer<T2, Self, OE2>
+    fn buffer<'or, 'sub, T, E, OE2>(self, boundary: OE2) -> Buffer<Self, OE2>
     where
         Self: Observable<'or, 'sub, T, E>,
-        OE2: Observable<'or, 'sub, T2, E>,
+        OE2: Observable<'or, 'sub, (), E>,
     {
         Buffer::new(self, boundary)
     }
--- tests/buffer.rs
+++ tests/buffer.rs
@@ -777,7 +777,7 @@ fn test_lifetime_or() {
             life_marker_1 = Some(observer);
             Subscription::new_none_disposal()
         });
-        let boundary_subject = Create::new(|observer: BoxedObserver<'_, (), _>| {
+        let boundary_subject = Create::new(|observer| {
             life_marker_2 = Some(observer);
             Subscription::new_none_disposal()
         });
@@ -796,8 +796,7 @@ fn test_clone() {
         observer.on_termination(Termination::Error(TestStruct));
         Subscription::new_none_disposal()
     });
-    let boundary_subject =
-        Create::new(|_: BoxedObserver<'_, (), _>| Subscription::new_none_disposal());
+    let boundary_subject = Create::new(|_| Subscription::new_none_disposal());
     let observable = observable.buffer(boundary_subject);
     _ = observable.clone(); // Make sure it's Clone when T and E are not Clone.
 }
@@ -806,7 +805,7 @@ fn test_clone() {
 fn test_type_inference_with_subscribe() {
     // Custom operations
     let subject: PublishSubject<'_, i32, String> = PublishSubject::default();
-    let boundary_subject: PublishSubject<'_, (), _> = PublishSubject::default();
+    let boundary_subject = PublishSubject::default();
     let observable = subject.buffer(boundary_subject);
 
     let observable = observable.buffer_with_count(1);
