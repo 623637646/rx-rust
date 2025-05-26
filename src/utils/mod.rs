99e7d8b029f3f46671de5479fab9b6c1d0895bbd
Yanni Wang <wy19900729@gmail.com>
2025-05-26 21:51:21 +0800

    subscribe_unsub_after_termination

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,2 +1,3 @@
 pub(crate) mod instant_lock;
 pub(crate) mod marker;
+pub mod unsub_after_termination;
---
+++ src/utils/unsub_after_termination.rs
@@ -0,0 +1,47 @@
+use crate::{
+    observer::{Observer, Termination},
+    subscription::Subscription,
+    utils::instant_lock::InstantMutLock,
+};
+use std::sync::{Arc, Mutex};
+
+pub fn subscribe_unsub_after_termination<'sub, OR, F>(
+    observer: OR,
+    builder: F,
+) -> Subscription<'sub>
+where
+    F: FnOnce(UnsubAfterTerminationObserver<'sub, OR>) -> Subscription<'sub>,
+{
+    let subscription = Arc::new(Mutex::new(None));
+    let observer = UnsubAfterTerminationObserver {
+        observer,
+        subscription: subscription.clone(),
+    };
+    *subscription.lock().unwrap() = Some(builder(observer));
+    Subscription::new_with_disposal_callback(move || {
+        if let Some(sub) = subscription.lock_mut(Option::take) {
+            sub.unsubscribe();
+        }
+    })
+}
+
+pub struct UnsubAfterTerminationObserver<'sub, OR> {
+    observer: OR,
+    subscription: Arc<Mutex<Option<Subscription<'sub>>>>,
+}
+
+impl<T, E, OR> Observer<T, E> for UnsubAfterTerminationObserver<'_, OR>
+where
+    OR: Observer<T, E>,
+{
+    fn on_next(&mut self, value: T) {
+        self.observer.on_next(value);
+    }
+
+    fn on_termination(self, termination: Termination<E>) {
+        self.observer.on_termination(termination);
+        if let Some(sub) = self.subscription.lock_mut(Option::take) {
+            sub.unsubscribe()
+        }
+    }
+}
