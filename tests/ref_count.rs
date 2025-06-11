50add051efb3996132448fd58ebc52dec469bd17
Yanni Wang <wy19900729@gmail.com>
2025-06-11 11:07:29 +0800

    Deadlock Tests for RefCount.

--- tests/ref_count.rs
+++ tests/ref_count.rs
@@ -4,9 +4,13 @@ use rx_rust::{
     observable::{Observable, observable_ext::ObservableExt, ref_count_observable::RefCount},
     observer::{Observer, Termination, boxed_observer::BoxedObserver},
     operators::creating::create::Create,
+    subject::publish_subject::PublishSubject,
     subscription::Subscription,
 };
-use std::convert::Infallible;
+use std::{
+    convert::Infallible,
+    sync::{Arc, Mutex},
+};
 use tests_utils::{checker::Checker, test_channel::test_channel, test_struct::TestStruct};
 
 #[test]
@@ -576,7 +580,333 @@ fn test_without_convenient_api() {
 }
 
 #[test]
-fn test_without_share_api() {
+fn test_complete_on_next() {
+    let mut counter = 0;
+    let mut subject = PublishSubject::default();
+    let (checker, observer) = Checker::new();
+
+    // Custom operations
+    let observable = subject.clone();
+    let observable = observable
+        .map(|_| {
+            counter += 1;
+            counter
+        })
+        .publish()
+        .ref_count();
+
+    let _subscription = observable.clone().subscribe(observer);
+    let mut subject_cloned = Some(subject.clone());
+    let _subscription = observable.subscribe_with_callback(
+        move |_| {
+            subject_cloned
+                .take()
+                .unwrap()
+                .on_termination(Termination::Completed);
+        },
+        move |_| {},
+    );
+    assert!(checker.values().is_empty());
+    assert!(checker.is_active());
+
+    subject.on_next(());
+    assert_eq!(checker.values(), [1]);
+    assert!(checker.is_completed());
+
+    subject
+        .clone()
+        .on_termination(Termination::<Infallible>::Completed);
+    assert_eq!(checker.values(), [1]);
+    assert!(checker.is_completed());
+}
+
+#[test]
+fn test_error_on_next() {
+    let mut counter = 0;
+    let mut subject = PublishSubject::default();
+    let (checker, observer) = Checker::new();
+
+    // Custom operations
+    let observable = subject.clone();
+    let observable = observable
+        .map(|_| {
+            counter += 1;
+            counter
+        })
+        .publish()
+        .ref_count();
+
+    let _subscription = observable.clone().subscribe(observer);
+    let mut subject_cloned = Some(subject.clone());
+    let _subscription = observable.subscribe_with_callback(
+        move |_| {
+            subject_cloned
+                .take()
+                .unwrap()
+                .on_termination(Termination::Error("error"));
+        },
+        move |_| {},
+    );
+    assert!(checker.values().is_empty());
+    assert!(checker.is_active());
+
+    subject.on_next(());
+    assert_eq!(checker.values(), [1]);
+    assert!(checker.is_error("error"));
+
+    subject.clone().on_termination(Termination::Completed);
+    assert_eq!(checker.values(), [1]);
+    assert!(checker.is_error("error"));
+}
+
+#[test]
+fn test_unsub_on_next() {
+    let mut counter = 0;
+    let (mut sender, observable, channel_checker) = test_channel::<'_, _, Infallible>();
+    let (checker_1, observer_1) = Checker::new();
+    let (checker_2, observer_2) = Checker::new();
+    let (checker_3, observer_3) = Checker::new();
+
+    // Custom operations
+    let observable = observable
+        .map(|_| {
+            counter += 1;
+            counter
+        })
+        .publish()
+        .ref_count();
+    let observable_1 = observable.clone();
+    let observable_2 = observable.clone();
+    let observable_3 = observable;
+
+    assert!(checker_1.values().is_empty());
+    assert!(checker_1.is_active());
+    assert!(checker_2.values().is_empty());
+    assert!(checker_2.is_active());
+    assert!(checker_3.values().is_empty());
+    assert!(checker_3.is_active());
+    assert!(channel_checker.is_initialized());
+
+    // no unsubscribe
+    let subscription = observable_1.subscribe(observer_1);
+
+    // unsubscribe before on_next
+    let sub = Arc::new(Mutex::new(None::<Subscription<'static>>));
+    let sub_cloned = sub.clone();
+    *sub.lock().unwrap() = Some(
+        observable_2
+            .hook_on_next(move |value, callback| {
+                sub_cloned.lock().unwrap().take().unwrap().unsubscribe();
+                callback(value);
+            })
+            .subscribe(observer_2),
+    );
+
+    // unsubscribe after on_next
+    let sub = Arc::new(Mutex::new(None::<Subscription<'static>>));
+    let sub_cloned = sub.clone();
+    *sub.lock().unwrap() = Some(
+        observable_3
+            .hook_on_next(move |value, callback| {
+                callback(value);
+                sub_cloned.lock().unwrap().take().unwrap().unsubscribe();
+            })
+            .subscribe(observer_3),
+    );
+
+    assert!(checker_1.values().is_empty());
+    assert!(checker_1.is_active());
+    assert!(checker_2.values().is_empty());
+    assert!(checker_2.is_active());
+    assert!(checker_3.values().is_empty());
+    assert!(checker_3.is_active());
+    assert!(channel_checker.is_subscribed());
+
+    sender.on_next(());
+    assert_eq!(checker_1.values(), [1]);
+    assert!(checker_1.is_active());
+    assert_eq!(checker_2.values(), [1]);
+    assert!(checker_2.is_dropped());
+    assert_eq!(checker_3.values(), [1]);
+    assert!(checker_3.is_dropped());
+    assert!(channel_checker.is_subscribed());
+
+    subscription.unsubscribe();
+    assert_eq!(checker_1.values(), [1]);
+    assert!(checker_1.is_dropped());
+    assert_eq!(checker_2.values(), [1]);
+    assert!(checker_2.is_dropped());
+    assert_eq!(checker_3.values(), [1]);
+    assert!(checker_3.is_dropped());
+    assert!(channel_checker.is_unsubscribed());
+}
+
+#[test]
+fn test_unsub_on_completed() {
+    let mut counter = 0;
+    let (mut sender, observable, channel_checker) = test_channel::<'_, _, Infallible>();
+    let (checker_1, observer_1) = Checker::new();
+    let (checker_2, observer_2) = Checker::new();
+    let (checker_3, observer_3) = Checker::new();
+
+    // Custom operations
+    let observable = observable
+        .map(|_| {
+            counter += 1;
+            counter
+        })
+        .publish()
+        .ref_count();
+    let observable_1 = observable.clone();
+    let observable_2 = observable.clone();
+    let observable_3 = observable;
+
+    assert!(checker_1.values().is_empty());
+    assert!(checker_1.is_active());
+    assert!(checker_2.values().is_empty());
+    assert!(checker_2.is_active());
+    assert!(checker_3.values().is_empty());
+    assert!(checker_3.is_active());
+    assert!(channel_checker.is_initialized());
+
+    // no unsubscribe
+    let _subscription = observable_1.subscribe(observer_1);
+
+    // unsubscribe before on_next
+    let sub = Arc::new(Mutex::new(None::<Subscription<'static>>));
+    let sub_cloned = sub.clone();
+    *sub.lock().unwrap() = Some(
+        observable_2
+            .hook_on_termination(move |value, callback| {
+                sub_cloned.lock().unwrap().take().unwrap().unsubscribe();
+                callback(value);
+            })
+            .subscribe(observer_2),
+    );
+
+    // unsubscribe after on_next
+    let sub = Arc::new(Mutex::new(None::<Subscription<'static>>));
+    let sub_cloned = sub.clone();
+    *sub.lock().unwrap() = Some(
+        observable_3
+            .hook_on_termination(move |value, callback| {
+                callback(value);
+                sub_cloned.lock().unwrap().take().unwrap().unsubscribe();
+            })
+            .subscribe(observer_3),
+    );
+
+    assert!(checker_1.values().is_empty());
+    assert!(checker_1.is_active());
+    assert!(checker_2.values().is_empty());
+    assert!(checker_2.is_active());
+    assert!(checker_3.values().is_empty());
+    assert!(checker_3.is_active());
+    assert!(channel_checker.is_subscribed());
+
+    sender.on_next(());
+    assert_eq!(checker_1.values(), [1]);
+    assert!(checker_1.is_active());
+    assert_eq!(checker_2.values(), [1]);
+    assert!(checker_2.is_active());
+    assert_eq!(checker_3.values(), [1]);
+    assert!(checker_3.is_active());
+    assert!(channel_checker.is_subscribed());
+
+    sender.on_termination(Termination::Completed);
+    assert_eq!(checker_1.values(), [1]);
+    assert!(checker_1.is_completed());
+    assert_eq!(checker_2.values(), [1]);
+    assert!(checker_2.is_completed());
+    assert_eq!(checker_3.values(), [1]);
+    assert!(checker_3.is_completed());
+    assert!(channel_checker.is_completed());
+}
+
+#[test]
+fn test_unsub_on_error() {
+    let mut counter = 0;
+    let (mut sender, observable, channel_checker) = test_channel();
+    let (checker_1, observer_1) = Checker::new();
+    let (checker_2, observer_2) = Checker::new();
+    let (checker_3, observer_3) = Checker::new();
+
+    // Custom operations
+    let observable = observable
+        .map(|_| {
+            counter += 1;
+            counter
+        })
+        .publish()
+        .ref_count();
+    let observable_1 = observable.clone();
+    let observable_2 = observable.clone();
+    let observable_3 = observable;
+
+    assert!(checker_1.values().is_empty());
+    assert!(checker_1.is_active());
+    assert!(checker_2.values().is_empty());
+    assert!(checker_2.is_active());
+    assert!(checker_3.values().is_empty());
+    assert!(checker_3.is_active());
+    assert!(channel_checker.is_initialized());
+
+    // no unsubscribe
+    let _subscription = observable_1.subscribe(observer_1);
+
+    // unsubscribe before on_next
+    let sub = Arc::new(Mutex::new(None::<Subscription<'static>>));
+    let sub_cloned = sub.clone();
+    *sub.lock().unwrap() = Some(
+        observable_2
+            .hook_on_termination(move |value, callback| {
+                sub_cloned.lock().unwrap().take().unwrap().unsubscribe();
+                callback(value);
+            })
+            .subscribe(observer_2),
+    );
+
+    // unsubscribe after on_next
+    let sub = Arc::new(Mutex::new(None::<Subscription<'static>>));
+    let sub_cloned = sub.clone();
+    *sub.lock().unwrap() = Some(
+        observable_3
+            .hook_on_termination(move |value, callback| {
+                callback(value);
+                sub_cloned.lock().unwrap().take().unwrap().unsubscribe();
+            })
+            .subscribe(observer_3),
+    );
+
+    assert!(checker_1.values().is_empty());
+    assert!(checker_1.is_active());
+    assert!(checker_2.values().is_empty());
+    assert!(checker_2.is_active());
+    assert!(checker_3.values().is_empty());
+    assert!(checker_3.is_active());
+    assert!(channel_checker.is_subscribed());
+
+    sender.on_next(());
+    assert_eq!(checker_1.values(), [1]);
+    assert!(checker_1.is_active());
+    assert_eq!(checker_2.values(), [1]);
+    assert!(checker_2.is_active());
+    assert_eq!(checker_3.values(), [1]);
+    assert!(checker_3.is_active());
+    assert!(channel_checker.is_subscribed());
+
+    sender.on_termination(Termination::Error("error"));
+    assert_eq!(checker_1.values(), [1]);
+    assert!(checker_1.is_error("error"));
+    assert_eq!(checker_2.values(), [1]);
+    assert!(checker_2.is_error("error"));
+    assert_eq!(checker_3.values(), [1]);
+    assert!(checker_3.is_error("error"));
+    assert!(channel_checker.is_error("error"));
+}
+
+#[test]
+fn test_share_api() {
     let mut counter = 0;
     let (mut sender, observable, channel_checker) = test_channel::<'_, _, Infallible>();
     let (checker_1, observer_1) = Checker::new();
