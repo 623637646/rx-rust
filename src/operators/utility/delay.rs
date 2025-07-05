2655ec5f846504202f7d7a2bb11a132bf665e6cf
Yanni Wang <wy19900729@gmail.com>
2025-07-05 15:03:06 +0800

    [BugFix] Delay can't stop in some cases.

--- src/operators/utility/delay.rs
+++ src/operators/utility/delay.rs
@@ -2,19 +2,18 @@ use crate::{
     observable::Observable,
     observer::{Observer, Termination},
     scheduler::Scheduler,
-    subscription::{Subscription, disposable::AutoDisposal},
+    subscription::{
+        Subscription,
+        disposable::{AutoDisposal, SharedDisposal},
+    },
 };
 use educe::Educe;
 use std::{
-    sync::{
-        Arc, Mutex,
-        atomic::{AtomicBool, Ordering},
-    },
-    time::Duration,
+    collections::VecDeque,
+    sync::{Arc, Mutex},
+    time::{Duration, Instant},
 };
 
-/// An observable that delays the next value and completed events from the source observable by a duration.
-/// The error will be emitted immediately.
 #[derive(Educe)]
 #[educe(Debug, Clone)]
 pub struct Delay<OE, S> {
@@ -24,13 +23,6 @@ pub struct Delay<OE, S> {
 }
 
 impl<OE, S> Delay<OE, S> {
-    /// Creates a new `Delay` observable.
-    ///
-    /// # Arguments
-    ///
-    /// * `source` - The source observable to delay.
-    /// * `delay` - The duration to delay each emission.
-    /// * `scheduler` - The scheduler to use for timing the delay.
     pub fn new(source: OE, delay: Duration, scheduler: S) -> Self {
         Self {
             source,
@@ -43,70 +35,102 @@ impl<OE, S> Delay<OE, S> {
 impl<'or, 'sub, T, E, OE, S> Observable<'static, 'sub, T, E> for Delay<OE, S>
 where
     T: Send + 'static,
-    E: Send + 'static,
     OE: Observable<'or, 'sub, T, E>,
     S: Scheduler + Send + 'or,
 {
     fn subscribe(self, observer: impl Observer<T, E> + Send + 'static) -> Subscription<'sub> {
+        let timer = Arc::new(Mutex::new(None));
         let delay_observer = DelayObserver {
             observer: Arc::new(Mutex::new(Some(observer))),
             delay: self.delay,
             scheduler: self.scheduler,
-            timers: Vec::new(),
+            values: Arc::new(Mutex::new(VecDeque::new())),
+            timer: timer.clone(),
         };
-        self.source.subscribe(delay_observer)
+        self.source.subscribe(delay_observer) + SharedDisposal::new(timer)
     }
 }
 
-struct DelayObserver<OR, S> {
+type DelayObserverValues<T> = Arc<Mutex<VecDeque<(Instant, Option<T>)>>>; // None means completed
+
+struct DelayObserver<T, OR, S> {
     observer: Arc<Mutex<Option<OR>>>,
     delay: Duration,
     scheduler: S,
-    timers: Vec<(AutoDisposal<'static>, Arc<AtomicBool>)>,
+    values: DelayObserverValues<T>,
+    timer: Arc<Mutex<Option<AutoDisposal<'static>>>>,
 }
 
-impl<T, E, OR, S> Observer<T, E> for DelayObserver<OR, S>
+impl<T, OR, S> DelayObserver<T, OR, S> {
+    fn setup_emit_timer_if_needed<E>(&mut self)
+    where
+        T: Send + 'static,
+        OR: Observer<T, E> + Send + 'static,
+        S: Scheduler,
+    {
+        if self.timer.lock().unwrap().is_some() {
+            return;
+        }
+        let values = self.values.clone();
+        let observer = self.observer.clone();
+        let timer = self.timer.clone();
+        *self.timer.lock().unwrap() = Some(self.scheduler.schedule_recursive(
+            move |_| {
+                if let Some((instant, value)) = { values.lock().unwrap().pop_front() } {
+                    if let Some(value) = value {
+                        //  next
+                        if let Some(observer) = observer.lock().unwrap().as_mut() {
+                            observer.on_next(value);
+                            if let Some((next_instant, _)) = values.lock().unwrap().front() {
+                                let delay = next_instant.duration_since(instant);
+                                Some(delay)
+                            } else {
+                                timer.lock().unwrap().take().unwrap();
+                                None
+                            }
+                        } else {
+                            timer.lock().unwrap().take().unwrap();
+                            None
+                        }
+                    } else {
+                        // completed
+                        if let Some(observer) = { observer.lock().unwrap().take() } {
+                            observer.on_termination(Termination::Completed);
+                        }
+                        timer.lock().unwrap().take().unwrap();
+                        None
+                    }
+                } else {
+                    unreachable!()
+                }
+            },
+            Some(self.delay),
+        ));
+    }
+}
+
+impl<T, E, OR, S> Observer<T, E> for DelayObserver<T, OR, S>
 where
     T: Send + 'static,
-    E: Send + 'static,
     OR: Observer<T, E> + Send + 'static,
     S: Scheduler,
 {
     fn on_next(&mut self, value: T) {
-        // clean up finished timers
-        self.timers
-            .retain(|(_, finished)| !finished.load(Ordering::SeqCst));
-
-        let observer = self.observer.clone();
-        let finished = Arc::new(AtomicBool::new(false));
-        let finished_cloned = finished.clone();
-        let timer = self.scheduler.schedule(
-            move || {
-                if let Some(observer) = observer.lock().unwrap().as_mut() {
-                    observer.on_next(value)
-                }
-                finished_cloned.store(true, Ordering::SeqCst);
-            },
-            Some(self.delay),
-        );
-        self.timers.push((timer, finished));
+        self.values
+            .lock()
+            .unwrap()
+            .push_back((Instant::now(), Some(value)));
+        self.setup_emit_timer_if_needed();
     }
 
-    fn on_termination(self, termination: Termination<E>) {
-        match &termination {
+    fn on_termination(mut self, termination: Termination<E>) {
+        match termination {
             Termination::Completed => {
-                let timer_holder = Arc::new(Mutex::new(None));
-                let timer_holder_cloned = timer_holder.clone();
-                *timer_holder.lock().unwrap() = Some(self.scheduler.schedule(
-                    move || {
-                        if let Some(observer) = { self.observer.lock().unwrap().take() } {
-                            observer.on_termination(termination);
-                        }
-                        drop(self.timers); // keep the timers alive
-                        drop(timer_holder_cloned); // keep the holder alive
-                    },
-                    Some(self.delay),
-                ));
+                self.values
+                    .lock()
+                    .unwrap()
+                    .push_back((Instant::now(), None));
+                self.setup_emit_timer_if_needed();
             }
             Termination::Error(_) => {
                 if let Some(observer) = { self.observer.lock().unwrap().take() } {
