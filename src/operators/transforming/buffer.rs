084b33b084c3ac55ff10c6ccc0e32a3ff6f92368
Yanni Wang <wy19900729@gmail.com>
2025-08-01 16:44:55 +0800

    [Improvement] Remove Clone from BufferObserver in Buffer.

--- src/operators/transforming/buffer.rs
+++ src/operators/transforming/buffer.rs
@@ -37,20 +37,18 @@ where
         observer: impl Observer<Vec<T>, E> + NecessarySend + 'or,
     ) -> Subscription<'sub> {
         subscribe_unsub_after_termination(observer, |observer| {
-            let observer = BufferObserver {
-                observer: Shared::new(Mutable::new(Some(observer))),
-                values: Shared::new(Mutable::new(Vec::default())),
-            };
-            let boundary = BoundaryObserver(observer.clone());
-            let subscription_1 = self.boundary.subscribe(boundary);
-            let subscription_2 = self.source.subscribe(observer);
+            let observer = Shared::new(Mutable::new(Some(observer)));
+            let values = Shared::new(Mutable::new(Vec::default()));
+            let subscription_1 = self.boundary.subscribe(BoundaryObserver {
+                observer: observer.clone(),
+                values: values.clone(),
+            });
+            let subscription_2 = self.source.subscribe(BufferObserver { observer, values });
             subscription_1 + subscription_2
         })
     }
 }
 
-#[derive(Educe)]
-#[educe(Clone)]
 struct BufferObserver<T, OR> {
     observer: Shared<Mutable<Option<OR>>>,
     values: Shared<Mutable<Vec<T>>>,
@@ -80,18 +78,32 @@ where
     }
 }
 
-struct BoundaryObserver<T, OR>(BufferObserver<T, OR>);
+struct BoundaryObserver<T, OR> {
+    observer: Shared<Mutable<Option<OR>>>,
+    values: Shared<Mutable<Vec<T>>>,
+}
 
 impl<T, E, OR> Observer<(), E> for BoundaryObserver<T, OR>
 where
     OR: Observer<Vec<T>, E>,
 {
     fn on_next(&mut self, _: ()) {
-        let values = self.0.values.safe_lock_mem_take();
-        self.0.observer.safe_lock_on_next_if_some(values);
+        let values = self.values.safe_lock_mem_take();
+        self.observer.safe_lock_on_next_if_some(values);
     }
 
     fn on_termination(self, termination: Termination<E>) {
-        self.0.on_termination(termination);
+        if let Some(mut observer) = self.observer.safe_lock_take() {
+            match termination {
+                Termination::Completed => {
+                    let values = self.values.safe_lock_mem_take();
+                    if !values.is_empty() {
+                        observer.on_next(values);
+                    }
+                }
+                Termination::Error(_) => {}
+            }
+            observer.on_termination(termination);
+        }
     }
 }
