24537df08f8d65ba7c85a2b4b34ead1c27206d50
Yanni Wang <wy19900729@gmail.com>
2025-08-09 23:22:46 +0800

    [BugFix] Remove useless lock of WindowWithCount.

--- src/operators/transforming/window_with_count.rs
+++ src/operators/transforming/window_with_count.rs
@@ -1,11 +1,10 @@
-use crate::utils::types::{Mutable, NecessarySend, Shared};
+use crate::utils::types::NecessarySend;
 use crate::{
     disposable::subscription::Subscription,
     observable::Observable,
     observer::{Observer, Termination},
     subject::{publish_subject::PublishSubject, subject_observable::SubjectObservable},
 };
-use crate::{safe_lock, safe_lock_observer};
 use educe::Educe;
 use std::{cmp::Ordering, num::NonZeroUsize};
 
@@ -40,7 +39,7 @@ where
 
         let observer = WindowWithCountObserver {
             observer,
-            subject: Shared::new(Mutable::new(subject)),
+            subject,
             count: self.count,
             sent_count: 0,
         };
@@ -50,7 +49,7 @@ where
 
 struct WindowWithCountObserver<'or, T, E, OR> {
     observer: OR,
-    subject: Shared<Mutable<PublishSubject<'or, T, E>>>,
+    subject: PublishSubject<'or, T, E>,
     count: NonZeroUsize,
     sent_count: usize,
 }
@@ -64,17 +63,14 @@ where
     fn on_next(&mut self, value: T) {
         match (self.sent_count + 1).cmp(&self.count.get()) {
             Ordering::Less => {
-                safe_lock_observer!(on_next: self.subject, value);
+                self.subject.on_next(value);
                 self.sent_count += 1;
             }
             Ordering::Equal => {
                 let new_subject = PublishSubject::default();
-                let old_subject = std::mem::replace(
-                    &mut self.subject,
-                    Shared::new(Mutable::new(new_subject.clone())),
-                );
-                safe_lock_observer!(on_next: old_subject, value);
-                safe_lock!(clone: old_subject).on_termination(Termination::Completed);
+                let mut old_subject = std::mem::replace(&mut self.subject, new_subject.clone());
+                old_subject.on_next(value);
+                old_subject.on_termination(Termination::Completed);
                 self.observer.on_next(SubjectObservable::new(new_subject));
                 self.sent_count = 0;
             }
@@ -83,7 +79,7 @@ where
     }
 
     fn on_termination(self, termination: Termination<E>) {
-        safe_lock!(clone: self.subject).on_termination(termination.clone());
+        self.subject.on_termination(termination.clone());
         self.observer.on_termination(termination);
     }
 }
