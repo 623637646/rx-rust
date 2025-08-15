78e2d2a252c080f976ae0d9c4113d1776283c2f7
Yanni Wang <wy19900729@gmail.com>
2025-08-15 16:11:41 +0800

    [BugFix][Tests] Make sure `alive_tasks_count.fetch_sub` do once in test_scheduler.rs

--- tests/tests_utils/test_scheduler.rs
+++ tests/tests_utils/test_scheduler.rs
@@ -1,55 +1,43 @@
 use crate::tests_utils::test_runtime::TestRuntime;
 use rx_rust::{
     disposable::{Disposable, callback_disposal::CallbackDisposal},
+    safe_lock_option,
     scheduler::Scheduler,
     utils::types::{Mutable, MutableHelper, NecessarySend, Shared},
 };
-use std::{
-    sync::atomic::{AtomicBool, Ordering},
-    time::Duration,
-};
+use std::{sync::atomic::Ordering, time::Duration};
 
 impl Scheduler for TestRuntime {
     fn schedule_future(
         &self,
         future: impl Future<Output = ()> + NecessarySend + 'static,
     ) -> impl Disposable + NecessarySend + 'static {
-        let is_finished = Shared::new(AtomicBool::new(false));
-        let is_finished_cloned = is_finished.clone();
         let alive_tasks_count = self.alive_tasks_count.clone();
-        let alive_tasks_count_cloned = alive_tasks_count.clone();
+        let count_sub = Shared::new(Mutable::new(Some(move || {
+            alive_tasks_count.fetch_sub(1, Ordering::SeqCst);
+        })));
+        let count_sub_cloned = count_sub.clone();
+
         let entry = Shared::new(Mutable::new(EntryExitChecker::enter()));
         let weak_entry = Shared::downgrade(&entry);
-        alive_tasks_count.fetch_add(1, Ordering::SeqCst);
+
         let future = async move {
             future.await;
             if let Some(entry) = weak_entry.upgrade() {
                 entry.lock_mut(|mut lock| EntryExitChecker::exit(&mut lock));
             }
-            let _ = is_finished.fetch_update(Ordering::SeqCst, Ordering::SeqCst, |is_finished| {
-                if !is_finished {
-                    alive_tasks_count.fetch_sub(1, Ordering::SeqCst);
-                    Some(true)
-                } else {
-                    None
-                }
-            });
+            if let Some(count_sub) = safe_lock_option!(take: count_sub) {
+                count_sub();
+            }
         };
+
+        self.alive_tasks_count.fetch_add(1, Ordering::SeqCst);
         let handle = self.spawn(future);
         CallbackDisposal::new(move || {
             entry.lock_mut(|mut lock| EntryExitChecker::exit(&mut lock));
-            let _ = is_finished_cloned.fetch_update(
-                Ordering::SeqCst,
-                Ordering::SeqCst,
-                |is_finished| {
-                    if !is_finished {
-                        alive_tasks_count_cloned.fetch_sub(1, Ordering::SeqCst);
-                        Some(true)
-                    } else {
-                        None
-                    }
-                },
-            );
+            if let Some(count_sub) = safe_lock_option!(take: count_sub_cloned) {
+                count_sub();
+            }
             handle.abort();
         })
     }
