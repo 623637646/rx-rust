3b69818e74a0aa5600f54f374a2cd958ec1759db
Yanni Wang <wy19900729@gmail.com>
2025-08-15 22:23:50 +0800

    [Tests] Change 10ms to 20ms for DURATION_DEVIATION.

--- tests/tests_utils/mod.rs
+++ tests/tests_utils/mod.rs
@@ -11,7 +11,7 @@ pub(crate) mod types;
 
 use std::time::Duration;
 pub(crate) const DURATION_NEXT_LOOP: Duration = Duration::from_millis(5);
-pub(crate) const DURATION_DEVIATION: Duration = Duration::from_millis(10);
+pub(crate) const DURATION_DEVIATION: Duration = Duration::from_millis(20);
 pub(crate) const DURATION_LOGICAL: Duration = Duration::from_millis(100);
 
 pub(crate) const RECURSION_EXECUTION_TIMES: usize = 200;
