7d2f453b1eca54c74a04261d5b9c50b478a8aaa2
Yanni Wang <wy19900729@gmail.com>
2025-09-05 17:05:25 +0800

    [Tests] Update test_until_error.sh

--- test_until_error.sh
+++ test_until_error.sh
@@ -18,7 +18,7 @@ while true; do
 
   echo "===== Loop #$count finished in ${elapsed}s ====="
 
-  if grep -q "test run failed" "$tmpfile"; then
+  if grep -q "process didn't exit successfully" "$tmpfile"; then
     mkdir -p logs
     timestamp=$(date +"%Y%m%d_%H%M%S")
     logfile="logs/test_${timestamp}_${count}.log"
