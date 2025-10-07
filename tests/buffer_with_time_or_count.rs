c26c168b322fca7196f19099020fadc72e7e8e0b
Yanni Wang <wy19900729@gmail.com>
2025-10-07 23:30:59 +0800

    [BugFix] Subscribe source first then do schedule_periodically in buffer_with_time_or_count.rs to make sure the fast values are emited.

--- tests/buffer_with_time_or_count.rs
+++ tests/buffer_with_time_or_count.rs
@@ -1892,16 +1892,24 @@ fn test_immediate_next() {
         // Custom operations
         let observable = subject.clone();
         let observable = observable.buffer_with_time_or_count(
-            NonZeroUsize::new(1).unwrap(),
+            NonZeroUsize::new(2).unwrap(),
             DURATION_100_MS,
             runtime.clone(),
-            Some(DURATION_100_MS),
+            None,
         );
 
         let _subscription = observable.subscribe(observer);
-        assert_eq!(checker.values(), [vec![111]]);
-        assert_eq!(checker.state(), State::Active);
-
+        check_with_spawned_late!(
+            runtime,
+            {
+                assert!(checker.values().is_empty());
+                assert_eq!(checker.state(), State::Active);
+            },
+            {
+                assert_eq!(checker.values(), [vec![111]]);
+                assert_eq!(checker.state(), State::Active);
+            }
+        );
         subject
             .clone()
             .on_termination(Termination::<Infallible>::Completed);
