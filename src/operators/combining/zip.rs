43b751188678cfa32d5051e280e245b31abb240a
Yanni Wang <wy19900729@gmail.com>
2026-06-30 23:26:13 +0800

    [Refactoring] Use subscribe_with_shared_model for Zip.

--- src/operators/combining/zip.rs
+++ src/operators/combining/zip.rs
@@ -1,5 +1,7 @@
-use crate::safe_lock_option_observer;
-use crate::utils::types::{Mutable, MutableHelper, NecessarySend, Shared};
+use crate::utils::subscribe_with_shared_model::{
+    Context, EventGroup, ModificationResult, subscribe_with_shared_model,
+};
+use crate::utils::types::NecessarySend;
 use crate::{
     disposable::subscription::Subscription,
     observable::Observable,
@@ -57,159 +59,121 @@ impl<OE1, OE2> Zip<OE1, OE2> {
 
 impl<'or, 'sub, T1, T2, E, OE1, OE2> Observable<'or, 'sub, (T1, T2), E> for Zip<OE1, OE2>
 where
+    'sub: 'or,
+    'or: 'sub,
     T1: NecessarySend + 'or,
     T2: NecessarySend + 'or,
+    E: NecessarySend + 'or,
     OE1: Observable<'or, 'sub, T1, E>,
     OE2: Observable<'or, 'sub, T2, E>,
-    'sub: 'or,
 {
     fn subscribe(
         self,
         observer: impl Observer<(T1, T2), E> + NecessarySend + 'or,
     ) -> Subscription<'sub> {
         subscribe_unsub_after_termination(observer, |observer| {
-            let observer = Shared::new(Mutable::new(Some(observer)));
-            let buffer = Shared::new(Mutable::new(ZipObserverBufferState::None));
-            let observer_1 = ZipObserver1 {
-                observer: observer.clone(),
-                buffer: buffer.clone(),
+            let model = Model {
+                first: (VecDeque::new(), false),
+                second: (VecDeque::new(), false),
             };
-            let observer_2 = ZipObserver2 { observer, buffer };
-            let subscription_1 = self.source_1.subscribe(observer_1);
-            let subscription_2 = self.source_2.subscribe(observer_2);
-            subscription_1 + subscription_2
+            subscribe_with_shared_model(observer, model, |context| {
+                let subscription_1 = self.source_1.subscribe(ZipObserver1(context.clone()));
+                let subscription_2 = self.source_2.subscribe(ZipObserver2(context));
+                subscription_1 + subscription_2
+            })
         })
     }
 }
 
-enum ZipObserverBufferState<T1, T2> {
-    None,
-    One {
-        buffer: VecDeque<T1>,
-        is_completed: bool,
-    },
-    Two {
-        buffer: VecDeque<T2>,
-        is_completed: bool,
-    },
+struct Model<T1, T2> {
+    first: (VecDeque<T1>, bool),  // bool means completed
+    second: (VecDeque<T2>, bool), // bool means completed
 }
 
-struct ZipObserver1<T1, T2, OR> {
-    observer: Shared<Mutable<Option<OR>>>,
-    buffer: Shared<Mutable<ZipObserverBufferState<T1, T2>>>,
-}
+struct ZipObserver1<T1, T2, E, OR>(Context<(T1, T2), E, OR, Model<T1, T2>>);
 
-impl<T1, T2, E, OR> Observer<T1, E> for ZipObserver1<T1, T2, OR>
+impl<T1, T2, E, OR> Observer<T1, E> for ZipObserver1<T1, T2, E, OR>
 where
     OR: Observer<(T1, T2), E>,
 {
     fn on_next(&mut self, value: T1) {
-        self.buffer.lock_mut(|mut lock| match &mut *lock {
-            ZipObserverBufferState::None => {
-                *lock = ZipObserverBufferState::One {
-                    buffer: VecDeque::from([value]),
-                    is_completed: false,
-                };
-            }
-            ZipObserverBufferState::One { buffer: items, .. } => {
-                items.push_back(value);
-            }
-            ZipObserverBufferState::Two {
-                buffer: items,
-                is_completed,
-            } => {
-                let item = items.pop_front().unwrap();
-                let should_complete;
-                if items.is_empty() {
-                    should_complete = *is_completed;
-                    *lock = ZipObserverBufferState::None;
+        let _ = self.0.modify_model(|model| {
+            let second = model.second.0.pop_front();
+            if let Some(second) = second {
+                if model.second.1 && model.second.0.is_empty() {
+                    ModificationResult::default().send_events(EventGroup::NextAndTermination(
+                        (value, second),
+                        Termination::Completed,
+                    ))
                 } else {
-                    should_complete = false;
+                    ModificationResult::new_send_next((value, second))
                 }
-                drop(lock);
-                if should_complete {
-                    safe_lock_option_observer!(on_next_and_termination: self.observer, (value, item), Termination::Completed);
-                } else {
-                    safe_lock_option_observer!(on_next: self.observer, (value, item));
-                }
+            } else {
+                model.first.0.push_back(value);
+                ModificationResult::default()
             }
         });
     }
 
     fn on_termination(self, termination: Termination<E>) {
-        let should_terminate = match termination {
-            Termination::Completed => self.buffer.lock_mut(|mut lock| match &mut *lock {
-                ZipObserverBufferState::None => true,
-                ZipObserverBufferState::One { is_completed, .. } => {
-                    *is_completed = true;
-                    false
-                }
-                ZipObserverBufferState::Two { .. } => true,
-            }),
-            Termination::Error(_) => true,
+        match termination {
+            Termination::Completed => {
+                let _ = self.0.modify_model(|model| {
+                    model.first.1 = true;
+                    if model.first.0.is_empty() {
+                        ModificationResult::new_send_termination(termination)
+                    } else {
+                        ModificationResult::default()
+                    }
+                });
+            }
+            Termination::Error(_) => {
+                self.0.send_termination(termination);
+            }
         };
-        if should_terminate {
-            safe_lock_option_observer!(on_termination: self.observer, termination);
-        }
     }
 }
 
-struct ZipObserver2<T1, T2, OR> {
-    observer: Shared<Mutable<Option<OR>>>,
-    buffer: Shared<Mutable<ZipObserverBufferState<T1, T2>>>,
-}
+struct ZipObserver2<T1, T2, E, OR>(Context<(T1, T2), E, OR, Model<T1, T2>>);
 
-impl<T1, T2, E, OR> Observer<T2, E> for ZipObserver2<T1, T2, OR>
+impl<T1, T2, E, OR> Observer<T2, E> for ZipObserver2<T1, T2, E, OR>
 where
     OR: Observer<(T1, T2), E>,
 {
     fn on_next(&mut self, value: T2) {
-        self.buffer.lock_mut(|mut lock| match &mut *lock {
-            ZipObserverBufferState::None => {
-                *lock = ZipObserverBufferState::Two {
-                    buffer: VecDeque::from([value]),
-                    is_completed: false,
-                };
-            }
-            ZipObserverBufferState::One {
-                buffer: items,
-                is_completed,
-            } => {
-                let item = items.pop_front().unwrap();
-                let should_complete;
-                if items.is_empty() {
-                    should_complete = *is_completed;
-                    *lock = ZipObserverBufferState::None;
+        let _ = self.0.modify_model(|model| {
+            let first = model.first.0.pop_front();
+            if let Some(first) = first {
+                if model.first.1 && model.first.0.is_empty() {
+                    ModificationResult::default().send_events(EventGroup::NextAndTermination(
+                        (first, value),
+                        Termination::Completed,
+                    ))
                 } else {
-                    should_complete = false;
+                    ModificationResult::new_send_next((first, value))
                 }
-                drop(lock);
-                if should_complete {
-                    safe_lock_option_observer!(on_next_and_termination: self.observer, (item, value), Termination::Completed);
-                } else {
-                    safe_lock_option_observer!(on_next: self.observer, (item, value));
-                }
-            }
-            ZipObserverBufferState::Two { buffer: items, .. } => {
-                items.push_back(value);
+            } else {
+                model.second.0.push_back(value);
+                ModificationResult::default()
             }
         });
     }
 
     fn on_termination(self, termination: Termination<E>) {
-        let should_terminate = match termination {
-            Termination::Completed => self.buffer.lock_mut(|mut lock| match &mut *lock {
-                ZipObserverBufferState::None => true,
-                ZipObserverBufferState::One { .. } => true,
-                ZipObserverBufferState::Two { is_completed, .. } => {
-                    *is_completed = true;
-                    false
-                }
-            }),
-            Termination::Error(_) => true,
+        match termination {
+            Termination::Completed => {
+                let _ = self.0.modify_model(|model| {
+                    model.second.1 = true;
+                    if model.second.0.is_empty() {
+                        ModificationResult::new_send_termination(termination)
+                    } else {
+                        ModificationResult::default()
+                    }
+                });
+            }
+            Termination::Error(_) => {
+                self.0.send_termination(termination);
+            }
         };
-        if should_terminate {
-            safe_lock_option_observer!(on_termination: self.observer, termination);
-        }
     }
 }
