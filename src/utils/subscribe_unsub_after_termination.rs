8132ff5d7947ab804d1eea6f76a781e48838af07
Yanni Wang <wy19900729@gmail.com>
2026-07-14 21:52:59 +0800

    [Improvement] Use SharedDisposal in subscribe_unsub_after_termination.

--- src/utils/subscribe_unsub_after_termination.rs
+++ src/utils/subscribe_unsub_after_termination.rs
@@ -1,35 +1,14 @@
 use crate::{
     delegate_disposal,
-    disposable::Disposable,
+    disposable::{Disposable, shared_disposal::SharedDisposal},
     observable::Subscription,
     observer::{Observer, Termination},
-    safe_lock,
-    utils::types::{Mutable, MutableHelper, Shared},
 };
 use educe::Educe;
 
-#[derive(Educe)]
-#[educe(Debug)]
-enum SubState<D: Disposable> {
-    Initialized,
-    Subscribed(Subscription<D>),
-    Unsubscribed,
-}
-
-// TODO: Disposable should not be Cloneable
-impl<D: Disposable> Disposable for Shared<Mutable<SubState<D>>> {
-    fn dispose(self) {
-        match safe_lock!(mem_replace: self, SubState::Unsubscribed) {
-            SubState::Initialized => {}
-            SubState::Subscribed(subscription) => subscription.dispose(),
-            SubState::Unsubscribed => {}
-        }
-    }
-}
-
 delegate_disposal!(
     Disposal<D>,
-    Shared<Mutable<SubState<D>>>,
+    SharedDisposal<Subscription<D>>,
     where D: Disposable
 );
 
@@ -42,36 +21,21 @@ where
     D: Disposable,
     F: FnOnce(UnsubAfterTerminationObserver<OR, D>) -> Subscription<D>,
 {
-    let sub_state = Shared::new(Mutable::new(SubState::Initialized));
+    let shared_disposal = SharedDisposal::default();
     let observer = UnsubAfterTerminationObserver {
         observer,
-        sub_state: sub_state.clone(),
+        shared_disposal: shared_disposal.clone(),
     };
-    let sub = builder(observer);
+    shared_disposal.replace(|| builder(observer));
 
-    sub_state.lock_mut(|mut lock| match &*lock {
-        SubState::Initialized => {
-            *lock = SubState::Subscribed(sub);
-            drop(lock);
-        }
-        SubState::Subscribed(_) => {
-            drop(lock);
-            unreachable!()
-        }
-        SubState::Unsubscribed => {
-            drop(lock);
-            sub.dispose()
-        }
-    });
-
-    sub_state.into()
+    shared_disposal.into()
 }
 
 #[derive(Educe)]
 #[educe(Debug)]
 pub struct UnsubAfterTerminationObserver<OR, D: Disposable> {
     observer: OR,
-    sub_state: Shared<Mutable<SubState<D>>>,
+    shared_disposal: SharedDisposal<Subscription<D>>,
 }
 
 impl<T, E, OR, D> Observer<T, E> for UnsubAfterTerminationObserver<OR, D>
@@ -85,6 +49,6 @@ where
 
     fn on_termination(self, termination: Termination<E>) {
         self.observer.on_termination(termination);
-        self.sub_state.dispose();
+        self.shared_disposal.dispose();
     }
 }
