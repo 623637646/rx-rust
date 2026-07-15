c2dd88d4d01b62b388e176b39a6e279536b8ce24
Yanni Wang <wy19900729@gmail.com>
2026-07-15 15:36:48 +0800

    [Refactoring] Use subscribe_with_context for take_until.

--- src/operators/conditional_boolean/take_until.rs
+++ src/operators/conditional_boolean/take_until.rs
@@ -1,19 +1,14 @@
 use crate::delegate_disposal;
-use crate::disposable::Disposable;
-use crate::disposable::chain_disposal::ChainDisposal;
-use crate::safe_lock_option_observer;
 use crate::utils::subscribe_with_auto_dispose_on_termination;
-use crate::utils::types::{MaybeSend, Mutable, Shared};
+use crate::utils::subscribe_with_context::{self, Context, subscribe_with_context};
+use crate::utils::types::MaybeSend;
 use crate::{
+    disposable::{Disposable, chain_disposal::ChainDisposal},
     observable::{Observable, Subscription},
     observer::{Observer, Termination},
-    utils::{
-        subscribe_with_auto_dispose_on_termination::subscribe_with_auto_dispose_on_termination,
-        types::MarkerType,
-    },
+    utils::subscribe_with_auto_dispose_on_termination::subscribe_with_auto_dispose_on_termination,
 };
 use educe::Educe;
-use std::marker::PhantomData;
 
 /// Emits the items emitted by a source Observable until a second Observable emits an item or a notification.
 /// See <https://reactivex.io/documentation/operators/takeuntil.html>
@@ -74,64 +69,60 @@ impl<OE, OE1> TakeUntil<OE, OE1> {
 }
 
 delegate_disposal!(
-    Disposal<D, D1>,
-    subscribe_with_auto_dispose_on_termination::Disposal<ChainDisposal<D, D1>>,
+    Disposal<'or, D, D1>,
+    subscribe_with_auto_dispose_on_termination::Disposal<subscribe_with_context::Disposal<'or, ChainDisposal<D, D1>>>,
     where D: Disposable, D1: Disposable
 );
 
 impl<'or, T, E, OE, OE1> Observable<'or, T, E> for TakeUntil<OE, OE1>
 where
-    T: 'or,
+    T: MaybeSend + 'or,
+    E: MaybeSend + 'or,
     OE: Observable<'or, T, E>,
     OE::D: MaybeSend + 'or,
     OE1: Observable<'or, (), E>,
     OE1::D: MaybeSend + 'or,
 {
-    type D = Disposal<OE::D, OE1::D>;
+    type D = Disposal<'or, OE::D, OE1::D>;
 
     fn subscribe(self, observer: impl Observer<T, E> + MaybeSend + 'or) -> Subscription<Self::D> {
         subscribe_with_auto_dispose_on_termination(observer, |observer| {
-            let observer = Shared::new(Mutable::new(Some(observer)));
-            let subscription_1 = self.stop.subscribe(StopObserver {
-                observer: observer.clone(),
-                _marker: PhantomData,
-            });
-            let subscription_2 = self.source.subscribe(TakeUntilObserver(observer));
-            subscription_1.preceded_by_bound(subscription_2)
+            subscribe_with_context(observer, (), |context| {
+                let subscription_1 = self.stop.subscribe(StopObserver(context.clone()));
+                let subscription_2 = self.source.subscribe(TakeUntilObserver(context));
+                subscription_1.preceded_by_bound(subscription_2)
+            })
         })
         .map_into()
     }
 }
 
-struct TakeUntilObserver<OR>(Shared<Mutable<Option<OR>>>);
+struct TakeUntilObserver<T, E, OR>(Context<T, E, OR, ()>);
 
-impl<T, E, OR> Observer<T, E> for TakeUntilObserver<OR>
+impl<T, E, OR> Observer<T, E> for TakeUntilObserver<T, E, OR>
 where
     OR: Observer<T, E>,
 {
     fn on_next(&mut self, value: T) {
-        safe_lock_option_observer!(on_next: self.0, value);
+        self.0.send_next(value);
     }
 
     fn on_termination(self, termination: Termination<E>) {
-        safe_lock_option_observer!(on_termination: self.0, termination);
+        self.0.send_termination(termination);
     }
 }
 
-struct StopObserver<T, OR> {
-    observer: Shared<Mutable<Option<OR>>>,
-    _marker: MarkerType<T>,
-}
+struct StopObserver<T, E, OR>(Context<T, E, OR, ()>);
 
-impl<T, E, OR> Observer<(), E> for StopObserver<T, OR>
+impl<T, E, OR> Observer<(), E> for StopObserver<T, E, OR>
 where
     OR: Observer<T, E>,
 {
     fn on_next(&mut self, _: ()) {
-        safe_lock_option_observer!(on_termination: self.observer, Termination::Completed);
+        self.0.send_termination(Termination::Completed);
     }
 
     fn on_termination(self, termination: Termination<E>) {
-        safe_lock_option_observer!(on_termination: self.observer, termination);
+        self.0.send_termination(termination);
     }
 }
